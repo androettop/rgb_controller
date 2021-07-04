bec52d6a04aee2751cde4731d67b4f087e0e597e
Pablo Androetto <androettop@gmail.com>
2021-07-04 16:35:33 -0300

    Breathe and rainbow effects

--- rgb_controller.ino
+++ rgb_controller.ino
@@ -1,15 +1,31 @@
+
 int pinR = 5;
 int pinG = 6;
 int pinB = 3;
+
+String lastCommand = "";
 
 int valueR = 0;
 int valueG = 0;
 int valueB = 0;
 
-
 int targetR= 0;
 int targetG = 0;
 int targetB = 0;
+
+int effect = 0;
+
+// Breath
+bool breathIn = true;
+double breathStepSize = 0.1;
+double breathStep = 0.1;
+double pi = 3.1416;
+
+// Rainbow
+int rainbowStep = 0;
+int colorDelay = 1000;
+int colorDelayCount = 0;
+
 
 void setup() {
   pinMode(pinR, OUTPUT);
@@ -20,17 +36,67 @@ void setup() {
 }
 
 void setTarget(){
-  if (Serial.available() > 0) {
-    String color = Serial.readString();
-    if(color.length() >= 9){
-      targetR = color.substring(0,3).toInt();
-      targetG = color.substring(3,6).toInt();
-      targetB = color.substring(6,9).toInt();
-    }
+  String color = lastCommand;
+  if(color.length() >= 9){
+    targetR = color.substring(0,3).toInt();
+    targetG = color.substring(3,6).toInt();
+    targetB = color.substring(6,9).toInt();
   }
 }
 
-void animateTarget(){
+void animateBreath(){
+  double multiplicator = (sin(breathStep)/2+0.5);
+  if (multiplicator >= 1){
+    multiplicator = 1;
+    breathIn = !breathIn;
+  }
+  if (multiplicator <= 0){
+    multiplicator = 1;
+    breathIn = !breathIn;
+  }
+  valueR = (int) (((double)targetR)*multiplicator);
+  valueG = (int) (((double)targetG)*multiplicator);
+  valueB = (int) (((double)targetB)*multiplicator);
+  
+  Serial.println(multiplicator);
+
+  if (breathIn){
+    breathStep += breathStepSize;
+  } else{
+    breathStep -= breathStepSize;
+  }
+  
+  delay(80);
+}
+
+void animateRainbow(){
+  //set color
+  if(colorDelayCount == 0){
+    targetR = 0;
+    targetG = 0;
+    targetB = 0;
+    switch (rainbowStep){
+      case 0:
+        targetR = 255;
+        break;
+      case 1:
+        targetG = 255;
+        break;
+      case 2:
+        targetB = 255;
+        break;
+    }
+  }
+  colorDelayCount += 10;
+  if(colorDelayCount >= colorDelay){
+    colorDelayCount = 0;
+    rainbowStep = (rainbowStep+1) % 3;
+  }  
+  animateFixed();
+}
+
+
+void animateFixed(){
   if(valueR < targetR){
     valueR++;
   }else if(valueR > targetR){
@@ -48,17 +114,44 @@ void animateTarget(){
   }else if(valueB > targetB){
     valueB--;  
   }
-  delay(4);
+  
+}
+
+void animate(){
+  switch(effect){
+    case 0:
+      animateFixed();
+      break;
+    case 1:
+      animateBreath();
+      break;
+    case 2:
+      animateRainbow();
+      break;
+  }
+}
+
+void setEffect(){
+  if( lastCommand.length() >= 1 && lastCommand.length() <= 2){
+    effect = lastCommand.toInt();
+  }
 }
 
 void displayColors(){
   analogWrite(pinR,valueR);
   analogWrite(pinG,valueG);
   analogWrite(pinB,valueB);
+  delay(4);
 }
 
 void loop() {
   displayColors();
+  
+  if (Serial.available() > 0) {
+    lastCommand = Serial.readString();
+  }
+   
   setTarget();
-  animateTarget();
+  setEffect();
+  animate();
 }
